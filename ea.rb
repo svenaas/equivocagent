cf729ace67f4550406809a1f33622267b71a3838
Sven Aas <sven.aas@gmail.com>
2014-04-15 19:30:22 -0400

    Add command-line options and usage. Add mention reactions (reply and follow).

--- ea.rb
+++ ea.rb
@@ -10,6 +10,8 @@ CHANCE_OF_TWEETING = 0.25
   config.access_token_secret = ENV["OAUTH_TOKEN_SECRET"]
 end
 
+@tweets_sent = 0
+
 def codephrase
 	# Picturable words from http://en.wiktionary.org/wiki/Appendix:Basic_English_word_list
 	nouns = %w{angle ant apple arch arm army baby bag ball band basin basket bath bed bee bell berry bird blade board boat bone book boot bottle box boy brain brake branch brick bridge brush bucket bulb button cake camera card cart carriage cat chain cheese chest chin church circle clock cloud coat collar comb cord cow cup curtain cushion dog door drain drawer dress drop ear egg engine eye face farm feather finger fish flag floor fly foot fork fowl frame garden girl glove goat gun hair hammer hand hat head heart hook horn horse hospital house island jewel kettle key knee knife knot leaf leg library line lip lock map match monkey moon mouth muscle nail neck needle nerve net nose nut office orange oven parcel pen pencil picture pig pin pipe plane plate plough pocket pot potato prison pump rail rat receipt ring rod roof root sail school scissors screw seed sheep shelf ship shirt shoe skin skirt snake sock spade sponge spoon spring square stamp star station stem stick stocking stomach store street sun table tail thread throat thumb ticket toe tongue tooth town train tray tree trousers umbrella wall watch wheel whip whistle window wing wire worm}
@@ -57,14 +59,84 @@ def location
 	location
 end
 
-if ARGV.size > 0
-  puts codephrase
-else
-	if rand < CHANCE_OF_TWEETING
-		begin
-		  @client.update codephrase, location
-		rescue Exception => e
-		  puts "An exception occured: #{e}"
-		end
+# Get the 20 newest followers
+def followers
+  @client.followers.take(20)
+end
+
+# Get the 20 most recent mentions
+def mentions
+	@client.mentions_timeline
+end
+
+def react_to_new_mentions
+	mentions.reverse.each do |m|
+		# Reply to mention unless this has already been done
+		tweet_codephrase(m) unless replied_to?(m)
+		# Try and follow mentioner
+		@client.follow(m.user)
 	end
 end
+
+def replied_to?(tweet)
+	tweets = @client.user_timeline(:since_id => tweet.id)
+	tweets.each do |t|
+		return true if t.in_reply_to_status_id == tweet.id
+	end
+	return false
+end
+
+def tweet_codephrase(in_reply_to = nil)
+	options = location
+	status  = codephrase
+
+	if in_reply_to
+		options = options.merge :in_reply_to_status_id => in_reply_to.id
+		status = "@#{in_reply_to.user.username} " + status
+	end
+
+	begin
+	  @client.update status, options
+	  @tweets_sent += 1
+	rescue Exception => e
+	  puts "An exception occured: #{e}"
+	end
+end
+
+def run
+	react_to_new_mentions
+	if @tweets_sent == 0 and rand < CHANCE_OF_TWEETING
+	  tweet_codephrase
+	end
+end
+
+def usage 
+	puts "Usage:"
+	puts "  ea.rb run        - normal execution      (may post to Twitter)"
+	puts "  ea.rb react      - react to new mentions (may post to Twitter)"
+	puts "  ea.rb codephrase - generate a random codephrase"
+	puts "  ea.rb location   - generate a random location"
+	puts "  ea.rb followers  - list 20 newest followers"
+	puts "  ea.rb mentions   - list 20 newest mentions"
+end
+
+if ARGV.size != 1
+	usage
+elsif ARGV[0] == 'run'
+	run
+elsif ARGV[0] == 'react'
+	react_to_new_mentions
+elsif ARGV[0] == 'codephrase'
+	puts codephrase
+elsif ARGV[0] == 'location'
+	puts location.to_s
+elsif ARGV[0] == 'followers'
+	followers.each {|f| puts "@#{f.username}"}
+elsif ARGV[0] == 'mentions'
+	mentions.each {|m| puts "@#{m.user.username}: #{m.full_text} (#{m.created_at}"}
+else 
+	usage
+end	
+
+#elsif ARGV[1] == 'run'
+#  run
